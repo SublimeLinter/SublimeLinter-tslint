e0afb26b4d4eca3b1239f13f023f0fa533751e12
herr kaste <herr.kaste@gmail.com>
2018-02-16 09:48:04 +0100

    Make plugin compatible with SL4
    I just typed this in GitHub. So a proper checkout and test is needed. You see that I try to branch between VERSION 3 or 4 here. (You may know or not, class bodies are executed in python unlike in js or ts.)

--- linter.py
+++ linter.py
@@ -13,6 +13,10 @@
 import os
 
 from SublimeLinter.lint import NodeLinter, util
+try:
+    from SublimeLinter.lint import VERSION
+except ImportError:
+    VERSION = 3
 
 
 class Tslint(NodeLinter):
@@ -26,30 +30,34 @@ class Tslint(NodeLinter):
         r'.+?\[(?P<line>\d+), (?P<col>\d+)\]: '
         r'(?P<message>.+)'
     )
-    error_stream = util.STREAM_BOTH
-    config_file = ('--config', 'tslint.json', '~')
     # tempfile_suffix = {'typescript': 'ts', 'typescriptreact': 'tsx'}
     tempfile_suffix = '-'
-    version_args = '--version'
-    version_requirement = '>= 5.8.0'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    
+    if VERSION > 3:
+        defaults = {
+            '--config': '${folder}/tslint.json',
+            '--project': '${folder}'
+        }    
 
-    def build_args(self, settings=None):
-        """Override build_args to allow setting a custom config filename."""
-        backup = self.config_file
-        if 'config_filename' in settings and self.filename:
-            self.config_file = self.config_file[:1] + (settings['config_filename'],) + self.config_file[2:]
+    if VERSION < 4:
+        config_file = ('--config', 'tslint.json', '~')
+        
+        def build_args(self, settings=None):
+            """Override build_args to allow setting a custom config filename."""
+            backup = self.config_file
+            if 'config_filename' in settings and self.filename:
+                self.config_file = self.config_file[:1] + (settings['config_filename'],) + self.config_file[2:]
 
-        out = super().build_args(settings)
+            out = super().build_args(settings)
 
-        projectPath = self.__findTSConfigPath()
-        if projectPath is not None:
-            out.extend(['--project', projectPath])
+            projectPath = self.__findTSConfigPath()
+            if projectPath is not None:
+                out.extend(['--project', projectPath])
 
-        # Reset the value of config_file so that this can apply per-project.
-        self.config_file = backup
+            # Reset the value of config_file so that this can apply per-project.
+            self.config_file = backup
 
-        return out
+            return out
 
-    def __findTSConfigPath(self):
-        return util.find_file(os.path.dirname(self.filename), 'tsconfig.json')
+        def __findTSConfigPath(self):
+            return util.find_file(os.path.dirname(self.filename), 'tsconfig.json')
