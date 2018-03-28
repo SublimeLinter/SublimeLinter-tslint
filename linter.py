30c3c7e24787d13f583a095be95a8170f5e48155
Koen Lageveen <koen.lageveen@me.com>
2018-03-28 15:48:18 +0200

    drop SL3 support, use selector, use SL conventions

--- linter.py
+++ linter.py
@@ -10,54 +10,20 @@
 
 """This module exports the Tslint plugin class."""
 
-import os
-
-from SublimeLinter.lint import NodeLinter, util
-try:
-    from SublimeLinter.lint import VERSION
-except ImportError:
-    VERSION = 3
+from SublimeLinter.lint import NodeLinter
 
 
 class Tslint(NodeLinter):
-    """Provides an interface to tslint."""
-
-    syntax = ('typescript', 'typescriptreact')
-    cmd = ('tslint', '@')
+    cmd = ('tslint', '${file}')
     npm_name = 'tslint'
     regex = (
         r'^(?:(?P<error>ERROR)|(?P<warning>WARNING))'
         r'.+?\[(?P<line>\d+), (?P<col>\d+)\]: '
         r'(?P<message>.+)'
     )
-    # tempfile_suffix = {'typescript': 'ts', 'typescriptreact': 'tsx'}
     tempfile_suffix = '-'
-    
-    if VERSION > 3:
-        defaults = {
-            '--config': '${folder}/tslint.json',
-            '--project': '${folder}'
-        }    
-
-    if VERSION < 4:
-        config_file = ('--config', 'tslint.json', '~')
-        
-        def build_args(self, settings=None):
-            """Override build_args to allow setting a custom config filename."""
-            backup = self.config_file
-            if 'config_filename' in settings and self.filename:
-                self.config_file = self.config_file[:1] + (settings['config_filename'],) + self.config_file[2:]
-
-            out = super().build_args(settings)
-
-            projectPath = self.__findTSConfigPath()
-            if projectPath is not None:
-                out.extend(['--project', projectPath])
-
-            # Reset the value of config_file so that this can apply per-project.
-            self.config_file = backup
-
-            return out
-
-        def __findTSConfigPath(self):
-            return util.find_file(os.path.dirname(self.filename), 'tsconfig.json')
+    defaults = {
+        'selector': 'source.ts',
+        '--config': '${folder}/tslint.json',
+        '--project': '${folder}'
+    }
