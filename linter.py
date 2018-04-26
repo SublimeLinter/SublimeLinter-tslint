f6694eaa2ef7ea110d22f92923f971219b04a69c
Koen Lageveen <koen.lageveen@me.com>
2018-04-26 11:33:11 +0200

    remove boilerplate comments

--- linter.py
+++ linter.py
@@ -1,15 +1,3 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Anton Lavrenov
-# Copyright (c) 2014 Anton Lavrenov
-#
-# License: MIT
-#
-
-"""This module exports the Tslint plugin class."""
-
 from SublimeLinter.lint import NodeLinter
 
 
